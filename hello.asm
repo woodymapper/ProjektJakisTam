54f11d0b1b19d8dfe2aac42945cc174c45896363
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-01 14:08:55 +0100

    PLACEHOLDER

---
+++ hello.asm
@@ -0,0 +1,20 @@
+hello.asm
+
+
+segment .data
+msg     db      "PLACEHOLDER", 0Ah    ; umieszcza w segmencie danych ciąg znaków zakończony znakiem końca linii
+
+segment .text
+        global  _start
+
+_start:
+        mov     eax, 4
+        mov     ebx, 1
+        mov     ecx, msg        
+        mov     edx, 14         
+        int     80h            
+
+
+        mov     eax, 1
+        xor     ebx, ebx
+        int     0x80
