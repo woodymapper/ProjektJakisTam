214a951c52e417c19e8571cdc828c80f41683368
aa <a@a>
2023-03-08 15:00:09 +0100

    modified:   composer.json 	modified:   composer.lock 	modified:   pub/.htaccess 	modified:   pub/index.php 	modified:   src/config.php 	new file:   src/templates/footer.html.twig 	new file:   src/templates/header.html.twig 	new file:   src/templates/index.html.twig 	new file:   src/templates/upload.html.twig

--- pub/index.php
+++ pub/index.php
@@ -5,8 +5,16 @@ require_once("./../src/config.php");
 use Steampixel\Route;
 
 Route::add("/",function(){
+//wyswietlanie
+global $twig;
+$twig->display("index.html.twig");
 
+});
+Route::add("/upload",function(){
+//strona z formularzem
 
+global $twig;
+$twig->display("upload.html.twig");
 
 });
 
--- src/config.php
+++ src/config.php
@@ -4,6 +4,9 @@ $db = new mysqli("localhost","root","","bazacms");
 
 require("Post.class.php");
 
+$loader = new Twig\Loader\FilesystemLoader('./../src/templates');
+//inicjujemy twiga
+$twig = new Twig\Environment($loader);
 
 
 ?>
