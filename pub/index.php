cc9c451b1465bc7ec376d381c2731e0d6fa5e6e0
aa <a@a>
2023-03-08 15:02:53 +0100

    modified:   pub/index.php

--- pub/index.php
+++ pub/index.php
@@ -4,19 +4,28 @@
 require_once("./../src/config.php");
 use Steampixel\Route;
 
-Route::add("/",function(){
+Route::add('/',function(){
 //wyswietlanie
 global $twig;
-$twig->display("index.html.twig");
+$twig->display('index.html.twig');
 
 });
-Route::add("/upload",function(){
+Route::add('/upload',function(){
 //strona z formularzem
 
 global $twig;
 $twig->display("upload.html.twig");
 
 });
+Route::add('/upload', function(){
+    require('./../src/config.php');
+    if(isset($_POST['submit'])) 
+        Post::upload($_FILES['uploadedFile']['tmp_name']);
+
+        $twig->display("index.html.twig");
+
+
+}, 'post');
 
 Route::run('ProjektJakisTam/pub/');
 ?>
