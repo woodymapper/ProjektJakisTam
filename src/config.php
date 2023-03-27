6ce432f0958dd49de037650c601e5978cdd49e26
woodymapper <70817697+woodymapper@users.noreply.github.com>
2023-03-27 12:00:41 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -9,5 +9,12 @@ $loader = new Twig\Loader\FilesystemLoader('./../src/templates');
 //inicjujemy twiga
 $twig = new Twig\Environment($loader);
 
-
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * I wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return woodymapper
+ * ----------------------------------------------------------------------------
+ */
 ?>
