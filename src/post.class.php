66b498a8f89c94c4dcce0f439019307ef5c5868a
aa <a@a>
2023-04-19 14:53:24 +0200

    modified:   src/post.class.php

--- src/post.class.php
+++ src/post.class.php
@@ -109,7 +109,7 @@ class Post {
 
         global $db;
         
-        $query = $db->prepare("INSERT INTO post VALUES(NULL, ?, ?, ?,0,?)");
+        $query = $db->prepare("INSERT INTO post VALUES(NULL, ?, ?,?,0,0,?)");
         
         $dbTimestamp = date("Y-m-d H:i:s");
         $titleString = $_POST['uploadedTitle'];
